2054434e3a4a217b8acd1237e16ba5d13c6bb55b
AntonIT99 <elsass.wolf@gmail.com>
2026-05-01 16:55:33 +0200

    Initial Commit

---
+++ src/test/java/com/wolfsnetz/webserver/WebserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wolfsnetz.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
